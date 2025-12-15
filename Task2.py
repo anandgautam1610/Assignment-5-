615af9cd8a0ea7eddc9638a5f1c0daa5075b0293
Anand Gautam <anandgautam1610@gmail.com>
2025-12-15 12:20:48 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,7 @@
+l1 = [1,2,3,4,5,6,7,8,9,10]
+print(f"Original list : {l1}")
+new_list = l1[0:5]
+print(f"Extracted first five elements : {new_list}")
+reversed_list = new_list[::-1]
+print(f"Reversed extracted elements : {reversed_list}")
+      
